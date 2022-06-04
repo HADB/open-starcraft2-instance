974441b100bdc5bcc4aba13331b13da2af2700d3
Bean <bean@yuanfen.net>
2022-06-04 10:02:17 +0800

    添加图标

--- OpenStarcraft2Instance/MainForm.Designer.cs
+++ OpenStarcraft2Instance/MainForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.label1 = new System.Windows.Forms.Label();
             this.gameFolderTextBox = new System.Windows.Forms.TextBox();
             this.openFolderButton = new System.Windows.Forms.Button();
@@ -85,6 +86,7 @@
             this.Controls.Add(this.gameFolderTextBox);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.71429F);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "星际2实例启动器";
